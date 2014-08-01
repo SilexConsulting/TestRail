53d90386f9f9fa53677f3d06883c36eddea69fa8
Jason Brown <jason@silex-consulting.com>
2014-08-01 14:08:40 +0100

    Add php script to list projects

---
+++ listprojects.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once 'testrail-api/php/testrail.php';
+
+set_exception_handler('exception_handler');
+main($argv);
+
+/**
+ * Perform list Project operation
+ *
+ * @param array $argv
+ */
+function main($argv)
+{
+  if (sizeof($argv) != 2) {
+    throw new Exception('usage: php listprojects.php <path_to_config_file>');
+  }
+
+  $config = get_config($argv[1]);
+}
+
+/**
+ * Parse an ini file into an associative array
+ *
+ * @param string $filename
+ * The filename of the ini file to be parsed
+ * @throws Exception
+ * @return array The settings are returned as an associative array on success, else FALSE on failure
+ */
+function get_config($filename)
+{
+  $config = parse_ini_file($filename);
+
+  if ($config === FALSE) {
+    throw new Exception("could not read configuration from '{$filename}'");
+  }
+
+  if (!isset($config['url'])) {
+    throw new Exception("url not found in '{$filename}'");
+  }
+  if (!isset($config['username'])) {
+    throw new Exception("username not found in '{$filename}'");
+  }
+  if (!isset($config['password'])) {
+    throw new Exception("password not found in '{$filename}'");
+  }
+  if (!isset($config['source'])) {
+    throw new Exception("source project not found in '{$filename}'");
+  }
+  if (!isset($config['destination'])) {
+    throw new Exception("destination project not found in '{$filename}'");
+  }
+
+  return $config;
+}
+
+$config = get_config($argv[1]);
+
+$testRail = new TestRailAPIClient($config['url']);
+$testRail->set_user($config['username']);
+$testRail->set_password($config['password']);
+
+//LIST PROJETCS
+header('Accept: application/json');
+$projects = $testRail->send_get('get_projects');
+
+var_dump(json_encode($projects));
+
+/**
+ * Exception-handler of last resort, writes to STDERR
+ *
+ * @param Exception $exception
+ */
+function exception_handler(Exception $exception)
+{
+  fwrite(STDERR, $exception->getMessage() . PHP_EOL);
+}
